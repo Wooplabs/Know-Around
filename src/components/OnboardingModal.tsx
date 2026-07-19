069779c3634d881abf0817ef7bb1d7e7ecf01060
Ajay <wooplabs@gmail.com>
2026-07-19 17:20:43 +0530

    OnboardingModal: Replace location arrow with round GPS target icon (locate-outline)

--- src/components/OnboardingModal.tsx
+++ src/components/OnboardingModal.tsx
@@ -13,7 +13,6 @@ import {
 } from 'react-native';
 import { useKnowAround } from '../context/KnowAroundContext';
 import { Ionicons } from '@expo/vector-icons';
-import { LocationIcon } from './CustomIcons';
 import * as Location from 'expo-location';
 
 export default function OnboardingModal() {
@@ -303,7 +302,7 @@ export default function OnboardingModal() {
                     <ActivityIndicator size="small" color="#1C873C" />
                   ) : (
                     <>
-                      <LocationIcon color="#1C873C" size={18} />
+                      <Ionicons name="locate-outline" size={18} color="#1C873C" />
                       <Text style={styles.ghostGpsBtnText}>At home? Use your current location</Text>
                     </>
                   )}
